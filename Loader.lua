407e264e11cba1d03ce5d496e5b0205a0a48d22c
mig0el787-arch <mig0el787@gmail.com>
2026-01-12 09:12:15 -0300

    Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,30 +1,13 @@
--- Ghoul Hub Loader ☠️ (SEM MÚSICA)
+print("Loader iniciou")
 
-print("Ghoul Hub Loader iniciado")
+local UI_URL = "https://raw.githubusercontent.com/mig0el787-arch/GhoulHub/main/UI.lua"
 
-if not game:IsLoaded() then
-	game.Loaded:Wait()
-end
-
-local Players = game:GetService("Players")
-local Player = Players.LocalPlayer
-local PlayerGui = Player:WaitForChild("PlayerGui")
-
--- Evitar duplicar
-if PlayerGui:FindFirstChild("GhoulHub") then
-	warn("Ghoul Hub já está carregado")
-	return
-end
-
--- Carregar UI
 local success, err = pcall(function()
-	loadstring(game:HttpGet(
-		"https://raw.githubusercontent.com/mig0el787-arch/GhoulHub/main/UI.lua"
-	))()
+	loadstring(game:HttpGet(UI_URL))()
 end)
 
 if success then
-	print("Ghoul Hub carregado com sucesso")
+	print("UI carregada com sucesso")
 else
-	warn("Erro ao carregar Ghoul Hub:", err)
+	warn("Erro ao carregar UI:", err)
 end
